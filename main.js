c8448adae4afa1aa11b8af0e60a69a577f181fda
fafi-svg <rfaelsolanob@gmail.com>
2023-07-30 11:13:41 -0500

    layout y expanded menu mobile-desing

---
+++ main.js
@@ -0,0 +1,12 @@
+function activar(){
+    document.getElementById("radio-register").checked=true;
+}
+// let menu_expanded = document.querySelectorAll("menu_expanded");
+// menu_expanded.forEach(function(item){
+//     if(item.id == "expanded-features"){
+//         item
+//     }
+//     if(item.id == "expanded-company"){
+
+//     }
+// })
