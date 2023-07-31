4aae4e8311b542b55d367f0215834f9fde374f5c
fafi-svg <rfaelsolanob@gmail.com>
2023-07-31 03:05:47 -0500

    fin

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
 function activar(){
     document.getElementById("radio-register").checked=true;
 }
-// let menu_expanded = document.querySelectorAll("menu_expanded");
-// menu_expanded.forEach(function(item){
-//     if(item.id == "expanded-features"){
-//         item
-//     }
-//     if(item.id == "expanded-company"){
-
-//     }
-// })
